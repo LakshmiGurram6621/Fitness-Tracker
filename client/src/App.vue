928b19a6435c924de46ad586764e9ab955ad84b6
Lakshmi Gurram <lakshmi@client137-118.wireless.newpaltz.edu>
2020-03-04 18:43:30 -0500

    committing first page design for fitness tracker application

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,43 @@
 <template>
-  <div id="app" class="container">
-    <figure class="image is-128x128">
-        <img src="https://tse2.mm.bing.net/th?id=OIP.2uUmassu3J-adt9zMH7CgQHaEK&pid=Api&P=0&w=312&h=176"></img>
-    </figure>
-  </div>  
+  <div >
+      <section class="hero is-primary is-fullwidth is-small">
+           <div class="hero-body">
+               <div class="container">
+                  <h1 class="title">
+                     Fitness Tracker For Women
+                  </h1>
+              </div>
+          </div>
+     </section>
+     <div>
+         <figure>
+             <img src="https://th.bing.com/th/id/OIP.iHFiIQGj92LVORsLgVBhUQHaHa?w=210&h=203&c=7&o=5&dpr=2&pid=1.7" style="width:1400px;height:480px;">
+         </figure>
+    </div>
+    <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+               <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                  is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          </p>
+       </div>
+   </footer>
+  </div>
 </template>
 
 <style lang="scss">
      @import "~bulma/css/bulma.css"
 </style>
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+// <script>
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'Home',
+//   components: {
+//     HelloWorld
+//   }
+// }
+// </script>
