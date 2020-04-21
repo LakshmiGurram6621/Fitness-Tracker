5b5feecec7cc2290fb972c848b208672c94ba28a
Lakshmi Gurram <lakshmi@Lakshmis-MacBook-Pro.local>
2020-04-20 22:08:40 -0400

    Committing server setting up changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const app=express();
 app.get('',(req,res)=>{
-    res.send("hello express");
+    res.send("hello expressJs");
 })
 app.listen(3000,()=>{
-    console.log("Im listening" );
+    console.log("Im listening to this port" );
 });
